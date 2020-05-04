0514b5e251d65c9f9e746a09683d7005971da9b8
skywind121 <46217270+skywind121@users.noreply.github.com>
2020-05-04 16:48:20 -0700

    Initial Commit

---
+++ HW3_00657051/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW3_00657051
+//
+//  Created by SW on 2020/5/4.
+//  Copyright © 2020 SW. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW3_00657051Tests/HW3_00657051Tests.swift
@@ -0,0 +1,34 @@
+//
+//  HW3_00657051Tests.swift
+//  HW3_00657051Tests
+//
+//  Created by SW on 2020/5/4.
+//  Copyright © 2020 SW. All rights reserved.
+//
+
+import XCTest
+@testable import HW3_00657051
+
+class HW3_00657051Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
